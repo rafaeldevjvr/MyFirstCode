cfd5207f1487349dbdfab8871b10dd7800f0ba16
Rafael Dev <rafaeldevprintln@gmail.com>
2024-07-13 11:55:53 -0300

    Variable Int

---
+++ VariaveisInt.java
@@ -0,0 +1,14 @@
+package MyFirstProgram;
+
+public class VariaveisInt {
+
+	public static void main(String[] args) {
+		
+		int x;
+		x = 150;
+		
+		System.out.println("Você tem R$" + x + " Reais");
+
+	}
+
+}
