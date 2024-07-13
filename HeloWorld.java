43948996daadf5234e64f8513c325ea24d39da0c
Rafael Dev <rafaeldevprintln@gmail.com>
2024-07-12 21:11:19 -0300

    Primeiro Commit

---
+++ HeloWorld.java
@@ -0,0 +1,12 @@
+package MyFirstProgram;
+
+public class HeloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		System.out.println("I love pizza");
+
+	}
+
+}
